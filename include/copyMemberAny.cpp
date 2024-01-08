a1e1d162d028efb874f6dd91ddc670242c4da09d
lijiaze <3035500479@qq.com>
2024-01-08 14:37:28 +0800

    change

--- include/copyMemberAny.cpp
+++ include/copyMemberAny.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 class Base {
 public:
-    Base( const std::string& data = "null" ) : m_data( data ) {}
+    Base( const std::string& data ) : m_data( data ) {}
+    Base()                               = default;
     Base( const Base& other )            = default;
     Base& operator=( const Base& other ) = default;
 
 protected:
     std::string m_data;
 };
-class Divered : public Base {
+class Derived : public Base {
 public:
-    Divered( const std::string& data = "null", const int& i = 0 ) : Base( data ), m_data1( i ) {}
-    Divered( const Divered& other ) : Base( other ), m_data1( other.m_data1 ) {}  // 1
-    Divered& operator=( const Divered& other ) {
+    Derived( const std::string& data, const int& i ) : Base( data ), m_data1( i ) {}
+    Derived( const Derived& other ) : Base( other ), m_data1( other.m_data1 ) {}  // 1
+    Derived& operator=( const Derived& other ) {
         Base::operator=( other );                                                 // 2
         m_data1 = other.m_data1;
         return *this;
     }
-    friend std::ostream& operator<<( std::ostream& os, const Divered& obj ) {
+    friend std::ostream& operator<<( std::ostream& os, const Derived& obj ) {
         std::cout << obj.m_data << " " << obj.m_data1;
         return os;
     }
@@ -26,10 +27,10 @@ private:
     int m_data1;
 };
 void test() {
-    Divered d{ "hello", 2 };
-    Divered d1{ d };
-    Divered d2{ "nihao", 3 };
-    Divered d3{ "world", 4 };
+    Derived d{ "hello", 2 };
+    Derived d1{ d };
+    Derived d2{ "nihao", 3 };
+    Derived d3{ "world", 4 };
     d2 = d;
     std::cout << d1 << std::endl;
     std::cout << d2 << std::endl;
